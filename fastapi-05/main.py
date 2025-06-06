1e3368c38c45dc2dbc304eef565257e11a59fae2
Daehyun <hatemogi@gmail.com>
2025-06-06 11:36:48 +0900

    05 예제 완성

--- fastapi-05/main.py
+++ fastapi-05/main.py
@@ -14,22 +14,26 @@ class ItemColor(str, Enum):
     green = "green"
     blue = "blue"
 
-class Item(BaseModel):
-    id: int
+class ItemBase(BaseModel):
     name: str = Field(description="상품명")
     color: Optional[ItemColor] = Field(default=None, description="상품 색상")
+
+class Item(ItemBase):
+    id: int
 
 temp_items = {
     1: Item(id=1, name="아이템A", color=ItemColor.red),
     2: Item(id=2, name="아이템B", color=ItemColor.green),
 }
 
+def item_or_404(item_id: int) -> Item:
+    if item_id not in temp_items:
+        raise HTTPException(status_code=404, detail="아이템이 없어요")
+    return temp_items[item_id]
+    
 @app.get("/items/{item_id}") # GET /items/{item_id}
 def read_item(item_id: int) -> Item:
-    if item_id in temp_items:
-        return temp_items[item_id]
-    else:
-        raise HTTPException(status_code=404, detail="아이템이 없어요")
+    return item_or_404(item_id)
 
 @app.post("/items", status_code=201, summary="2.1 상품등록 0516") # POST /items
 def create_item(item: Item):
@@ -37,3 +41,17 @@ def create_item(item: Item):
         raise HTTPException(status_code=400, detail="아이템이 이미 있어요")
     temp_items[item.id] = item
     return item
+
+@app.put("/items/{item_id}", status_code=202)
+def update_item(item_id: int, new_item: ItemBase):
+    item = item_or_404(item_id)
+    item.name = new_item.name
+    item.color = new_item.color
+    return {"updated": True}
+
+@app.delete("/items/{item_id}", status_code=204)
+def delete_item(item_id: int):
+    _ = item_or_404(item_id)
+    del temp_items[item_id]
+    return {"deleted": True}
+
