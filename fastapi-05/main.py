7b6e5bb879ba67dda6142af1e967bfa816617c68
Daehyun <hatemogi@gmail.com>
2025-06-11 12:17:59 +0900

    nanoid 적용

--- fastapi-05/main.py
+++ fastapi-05/main.py
@@ -1,13 +1,16 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel, Field
 from enum import Enum
-from typing import Optional
+from typing import Optional, Dict
+import nanoid
 
 app = FastAPI()
 
 @app.get("/") # GET /
 def root():
     return {"message": "Hello World"}
+
+type ItemId = str
 
 class ItemColor(str, Enum):
     red = "red"
@@ -19,39 +22,40 @@ class ItemBase(BaseModel):
     color: Optional[ItemColor] = Field(default=None, description="상품 색상")
 
 class Item(ItemBase):
-    id: int
+    id: ItemId
 
-temp_items = {
-    1: Item(id=1, name="아이템A", color=ItemColor.red),
-    2: Item(id=2, name="아이템B", color=ItemColor.green),
-}
+temp_items: Dict[ItemId, Item] = {item.id: item for item in [
+    Item(id="qRhBXnpry7", name="아이템A", color=ItemColor.red),
+    Item(id="cOPAmgTBfc", name="아이템B", color=ItemColor.green),
+]}
 
-def item_or_404(item_id: int) -> Item:
+def item_or_404(item_id: ItemId) -> Item:
     if item_id not in temp_items:
         raise HTTPException(status_code=404, detail="아이템이 없어요")
     return temp_items[item_id]
-    
+
 @app.get("/items/{item_id}") # GET /items/{item_id}
-def read_item(item_id: int) -> Item:
+def read_item(item_id: ItemId) -> Item:
     return item_or_404(item_id)
 
 @app.post("/items", status_code=201, summary="2.1 상품등록 0516") # POST /items
-def create_item(item: Item):
-    if item.id in temp_items:
-        raise HTTPException(status_code=400, detail="아이템이 이미 있어요")
-    temp_items[item.id] = item
+def create_item(item: ItemBase):
+    id: ItemId = nanoid.generate(size=10)
+    if id in temp_items:
+        raise HTTPException(status_code=407, detail="아이템이 이미 있어요")
+    item = Item(**item.model_dump(), id=id)
+    temp_items[id] = item
     return item
 
 @app.put("/items/{item_id}", status_code=202)
-def update_item(item_id: int, new_item: ItemBase):
+def update_item(item_id: ItemId, new_item: ItemBase):
     item = item_or_404(item_id)
     item.name = new_item.name
     item.color = new_item.color
     return {"updated": True}
 
 @app.delete("/items/{item_id}", status_code=204)
-def delete_item(item_id: int):
+def delete_item(item_id: ItemId):
     _ = item_or_404(item_id)
     del temp_items[item_id]
     return {"deleted": True}
-
